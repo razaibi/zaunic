1dafda2ce22afa684a68d20133858a5ae856ca82
Raza Balbale <ribalbale@gmail.com>
2021-05-26 18:09:31 +0000

    Moved core and added data type mapper.

---
+++ autocore/core.py
@@ -0,0 +1,93 @@
+import os
+import jinja2
+import yaml
+import template_logic
+from progress.bar import ChargingBar
+import CONFIGS
+
+
+def read_template(template_type, template_name, params):
+    _search_path = os.path.join('templates','{}'.format(template_type))
+    template_loader = jinja2.FileSystemLoader(searchpath=_search_path)
+    template_env = jinja2.Environment(loader=template_loader)
+    TEMPLATE_FILE = "{}.j2".format(template_name)
+    template = template_env.get_template(TEMPLATE_FILE)
+    outputText = template.render(params)
+    return outputText
+
+
+def read_yaml(file_path):
+    documents=None
+    with open(file_path) as file:
+        documents = yaml.load(file)
+    return documents
+
+def write_to_file(filename, content):
+    file_path = filename
+    if not os.path.exists(os.path.dirname(file_path)):
+        try:
+            os.makedirs(os.path.dirname(file_path))
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    with open(file_path, "w+") as f:
+        f.write(content)
+
+
+def process_template(
+        **task
+    ):
+    _injector = read_yaml(
+        os.path.join(
+            "source_data",
+            "{}.yml".format( task['source_data'])
+        )
+    )
+
+    _rendered_template = read_template(
+            task['category'],
+            task['template'],
+            _injector['data']
+    )
+
+    output_file = os.path.join(
+        CONFIGS.OUTPUT_FOLDER,
+        '{}'.format(task['output'])
+    )
+
+    write_to_file(
+        output_file,
+        _rendered_template
+    )
+
+
+def process_playbook(playbook_name):
+    
+    playbook_data = read_yaml(
+        os.path.join(
+            CONFIGS.PLAYBOOKS_FOLDER,
+            '{}.yml'.format(playbook_name)
+        )
+    )
+    bar = ChargingBar(
+        'Processing {}'.format(playbook_name), 
+        max=len(playbook_data)
+    )
+    for task in playbook_data['tasks']:
+        process_template(
+            **task
+        )
+        bar.next()
+    bar.finish()
+
+def autocode():
+    playbook_list = read_yaml(
+        os.path.join(
+            CONFIGS.RUNNER_FOLDER,
+            '{}.yml'.format('running_list')
+        )
+    )
+    for playbook in playbook_list['playbooks']:
+        process_playbook(playbook)
+
+
---
+++ autocore/data_mapper.py
@@ -0,0 +1,56 @@
+datatypes = {
+    "int" : {
+        "postgresql" : "INT",
+        "sqlserver" : "INT",
+        "c_sharp" : "Int32",
+        "pydantic" : "int",
+        "elixir" : "",
+        "java" : "int",
+        "javascript" : ""
+    },
+    "bigint" : {
+        "postgresql" : "BIGINT",
+        "sqlserver" : "BIGINT",
+        "c_sharp" : "Int64",
+        "pydantic" : "int",
+        "elixir" : "",
+        "java" : "long",
+        "javascript" : ""
+    },
+    "boolean" : {
+        "postgresql" : "BOOLEAN",
+        "sqlserver" : "BIT",
+        "c_sharp" : "bool",
+        "pydantic" : "int",
+        "elixir" : "",
+        "java" : "boolean",
+        "javascript" : ""
+    },
+    "char" : {
+        "postgresql" : "CHAR",
+        "sqlserver" : "CHAR",
+        "c_sharp" : "char",
+        "pydantic" : "str",
+        "elixir" : "",
+        "java" : "char",
+        "javascript" : ""
+    },
+    "varchar" : {
+        "postgresql" : "VARCHAR",
+        "sqlserver" : "VARCHAR",
+        "c_sharp" : "string",
+        "pydantic" : "str",
+        "elixir" : "",
+        "java" : "String",
+        "javascript" : ""
+    },
+    "text" : {
+        "postgresql" : "TEXT",
+        "sqlserver" : "VARCHAR",
+        "c_sharp" : "string",
+        "pydantic" : "str",
+        "elixir" : "",
+        "java" : "String",
+        "javascript" : ""
+    }
+}
