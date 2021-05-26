036dfbd4ccf6492c40eb04a71f7f59e2c358e5be
Raza Balbale <ribalbale@gmail.com>
2021-05-26 21:08:02 +0000

    Added placeholder for Logic Mapper.

---
+++ autocore/logic_mapper.py
@@ -0,0 +1,6 @@
+# Setup names that you would use in your "source_data" files
+# to invoke or apply logic/ logical conditions on the data.
+
+logic_map = {
+    "Clean Postgresql Table" : "postgresql_clean_table"
+}
