ae72f31ce750f62499db7e64c9b84c417a3b6c47
Raza Balbale <ribalbale@gmail.com>
2021-05-27 02:58:45 +0000

    Added references to template logic.

--- autocore/logic_mapper.py
+++ autocore/logic_mapper.py
@@ -1,6 +1,8 @@
 # Setup names that you would use in your "source_data" files
 # to invoke or apply logic/ logical conditions on the data.
+from template_logic import postgresql
+
 
 logic_map = {
-    "Clean Postgresql Table" : "postgresql_clean_table"
+    "clean_postgresql_columns" : postgresql.clean_postgresql_ddl_data
 }
