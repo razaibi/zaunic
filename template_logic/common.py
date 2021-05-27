25d38a3c2180416ee594cee540fc9130a943b4b5
Raza Balbale <ribalbale@gmail.com>
2021-05-27 02:56:24 +0000

    Added dynamic references from CONFIGS file.

--- template_logic/common.py
+++ template_logic/common.py
@@ -1,9 +1,8 @@
 from autocore import data_mapper
+import CONFIGS
 
 def process_common_logic(category, data):
     data = process_datatype_logic(category, data)
-
-
     return data
 
 
@@ -14,17 +13,15 @@ def process_datatype_logic(category, data):
         'dotnet-webapi',
         'fastapi'
     ]
-
     if category in type_based_solutions:
-        for db in data['databases']:
-            for table in db['tables']:
-                for column in table['columns']:
+        for db in data[CONFIGS.SOLUTIONS_TERM]:
+            for table in db[CONFIGS.ENTITIES_TERM]:
+                for column in table[CONFIGS.ATTRIBUTES_TERM]:
                     column['type'] = map_data_type(
                         column['type'],
                         category
                     )
     return data
-
 
 def map_data_type(generic_type, target_category):
     datatypes = data_mapper.datatypes
