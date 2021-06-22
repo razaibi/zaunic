544064579deeddc129d9381d5573d2d621f41a3e
Raza Balbale <ribalbale@gmail.com>
2021-06-22 13:26:38 +0000

    Added feature to support short templates (YAML based).

--- autocore/core.py
+++ autocore/core.py
@@ -1,6 +1,8 @@
 import os
 import jinja2
+from jinja2 import Template
 import yaml
+import errno
 from template_logic import common
 from autocore.logic_mapper import logic_map
 from progress.bar import ChargingBar
@@ -8,7 +10,10 @@ import CONFIGS
 
 
 def read_template(template_type, template_name, params):
-    _search_path = os.path.join('templates','{}'.format(template_type))
+    _search_path = os.path.join(
+        CONFIGS.TEMPLATES_FOLDER,
+        '{}'.format(template_type)
+    )
     template_loader = jinja2.FileSystemLoader(searchpath=_search_path)
     template_env = jinja2.Environment(loader=template_loader)
     TEMPLATE_FILE = "{}.j2".format(template_name)
@@ -16,11 +21,35 @@ def read_template(template_type, template_name, params):
     outputText = template.render(params)
     return outputText
 
+def read_short_template(template_type, nested_file_name, template_name, params):
+    """
+    Construct path template path.
+    """
+    template_path = os.path.join(
+        CONFIGS.TEMPLATES_FOLDER,
+        template_type,
+        ".".join([
+            nested_file_name,
+            CONFIGS.SHORT_TEMPLATE_FORMAT
+        ])        
+    )
+
+    """
+    Extract specific template.
+    """
+    template_collection = read_yaml(template_path)
+    template_raw = Template(template_collection[template_name])
+    """
+    Render template.
+    """
+    outputText = template_raw.render(params)
+    return outputText
+
 
 def read_yaml(file_path):
     documents=None
     with open(file_path) as file:
-        documents = yaml.load(file)
+        documents = yaml.load(file, Loader=yaml.FullLoader)
     return documents
 
 def write_to_file(filename, content):
@@ -31,8 +60,12 @@ def write_to_file(filename, content):
         except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise
-    with open(file_path, "w+") as f:
-        f.write(content)
+        with open(file_path, "w+") as f:
+            f.write(content)
+    else:
+        with open(file_path, "a") as f:
+            f.write(content)
+        
 
 
 def process_template(
@@ -53,15 +86,30 @@ def process_template(
     )
 
     #Apply logic specified in the source data file.
-    data_injector['data'] = process_custom_logic(data_injector['data'])
+    #data_injector['data'] = process_custom_logic(data_injector['data'])
 
     #Render template by injecting data.
-    _rendered_template = read_template(
+    ## Check if Template is individual or nested.
+    if CONFIGS.NESTED_TEMPLATE_SEPARATOR in task['template']:
+        _template_file_parts = task["template"].split(
+            CONFIGS.NESTED_TEMPLATE_SEPARATOR
+        )
+        _nested_file = _template_file_parts[0]
+        _jinja_template_name = _template_file_parts[1]
+        _rendered_template = read_short_template(
             task['category'],
-            task['template'],
+            _nested_file,
+            _jinja_template_name,
             data_injector['data']
-    )
+        )
+    else:
+        _rendered_template = read_template(
+                task['category'],
+                 task['template'],
+                data_injector['data']
+        )
 
+    #Prepare output
     output_file = os.path.join(
         CONFIGS.OUTPUT_FOLDER,
         '{}'.format(task['output'])
@@ -72,13 +120,13 @@ def process_template(
         _rendered_template
     )
 
-def process_custom_logic(data):
-    #Loop through solutions (databases)
-    for solution in data[CONFIGS.SOLUTIONS_TERM]:
-        if 'logic' in solution:
-            for logic_item in solution['logic']:
-                data = logic_map[logic_item](data)
-    return data
+#def process_custom_logic(data):
+#    #Loop through solutions (databases)
+#    for solution in data[CONFIGS.SOLUTIONS_TERM]:
+#        if 'logic' in solution:
+#            for logic_item in solution['logic']:
+#                data = logic_map[logic_item](data)
+#    return data
 
 def process_playbook(playbook_name):
     playbook_data = read_yaml(
