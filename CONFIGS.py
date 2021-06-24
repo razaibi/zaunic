6d7a1ce6bc389ba4aec1e1dba9dfa01ffa38c957
razaibi <ribalbale@gmail.com>
2021-06-24 08:22:27 -0500

    Organized configs for readability.

--- CONFIGS.py
+++ CONFIGS.py
@@ -1,13 +1,25 @@
-PLAYBOOKS_FOLDER="playbooks"
+# Runner Configs
 RUNNER_FOLDER = "runner"
+
+# Output Configs
 OUTPUT_FOLDER = 'output'
+
+# Data Configs
+SOURCE_DATA_FOLDER = 'data'
+
+# Template Configs
+SHORT_TEMPLATE_FORMAT = "yml"
+TEMPLATES_FOLDER = 'templates'
+
+# Logic Configs
 SOLUTIONS_TERM = 'databases'
 ENTITIES_TERM = 'tables'
 ATTRIBUTES_TERM = 'columns'
+
+# Playbook Configs
+
+PLAYBOOKS_FOLDER="playbooks"
 NESTED_TEMPLATE_SEPARATOR = ">"
-SHORT_TEMPLATE_FORMAT = "yml"
-TEMPLATES_FOLDER = 'templates'
-SOURCE_DATA_FOLDER = 'data'
-## Do not use period (.) for OUTPUT_FOLDER_SEPARATOR
 OUTPUT_FOLDER_SEPARATOR = ">"
+## Do not use period (.) for OUTPUT_FOLDER_SEPARATOR
 DATA_FOLDER_SEPARATOR=">"
