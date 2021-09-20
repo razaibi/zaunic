25f9480b3d74e344a76dba8232b6a21f46494e89
Raza Balbale <ribalbale@gmail.com>
2021-09-20 17:16:17 -0500

    Cleaned typer boilerplate.

--- main.py
+++ main.py
@@ -23,16 +23,5 @@ def run_all():
     #sm.add_secret('dev','client_id', 'S0m#$up@_!n2erest!ng$*cre7')
     #sm.get_secret('dev','client_id')
 
-@app.command()
-def hello(name: str = typer.Option(..., "--name", "--n")):
-    typer.echo(f"Hello {name}")
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
-
 if __name__ == "__main__":
     app()
