ca377693aa93f6b74db7dde48cdb8dce1c267d52
rRaza Balbale <ribalbale@gmail.com>
2021-10-15 21:06:48 -0500

    Cleaned up core.py.

--- zcore/core.py
+++ zcore/core.py
@@ -1,14 +1,6 @@
 import os
 from zcore.processor.manager import Factory as ProcessorFactory
 
-#def process_custom_logic(data):
-#    #Loop through solutions (databases)
-#    for solution in data[CONFIGS.SOLUTIONS_TERM]:
-#        if 'logic' in solution:
-#            for logic_item in solution['logic']:
-#                data = logic_map[logic_item](data)
-#    return data
-
 def run():
     playbook_processor_service = ProcessorFactory("playbook")
     playbook_list = playbook_processor_service.read_list()
