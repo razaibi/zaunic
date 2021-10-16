73cee6c1d428380f47258fc0a3f5009eeeca45a2
rRaza Balbale <ribalbale@gmail.com>
2021-10-15 21:02:29 -0500

    Cleaned up taskflow service.

--- zcore/processor/playbook_service.py
+++ zcore/processor/playbook_service.py
@@ -7,6 +7,7 @@ from zcore.secret.orchestrator_service import OrchestratorService
 from zcore.processor.service import ProcessorService
 from zcore.action.generator_service import GeneratorActionService
 from zcore.action.executor_service import ExecutorActionService
+from zcore.action.file_service import FileActionService
 from progress.bar import ChargingBar
 
 class PlaybookProcessorService(ProcessorService):
@@ -62,18 +63,16 @@ class PlaybookProcessorService(ProcessorService):
         playbook_data = self.impute_secrets(playbook_data)
         result = [PlaybookProcessorService.perform_action(node, node_processor_service, playbook_name, playbook_data) for node in node_list]
 
-        #for node in node_list:
-        #    #node
-
     @staticmethod
     def action_switcher(params_dict) -> None:
         action = {
-           'generate' : PlaybookProcessorService.call_generate_action, 
-           'copy' : PlaybookProcessorService.call_generate_action, 
-           'call_api' : PlaybookProcessorService.call_generate_action, 
-           'execute' : PlaybookProcessorService.call_execute_action
+           'generate' : GeneratorActionService.generate_content_remote, 
+           'upload' : FileActionService.upload_file, 
+           'download' : FileActionService.download_file, 
+           'call_api' : FileActionService.download_file, 
+           'execute' : ExecutorActionService.execute_command
         }
-        action[params_dict['task']['action']](**params_dict)
+        action[params_dict['task']['action']](params_dict)
 
     @staticmethod
     def perform_action(node, node_processor_service, playbook_name, playbook_data):
@@ -94,74 +93,29 @@ class PlaybookProcessorService(ProcessorService):
             ## For Generation tasks, invoke generator service.
 
             for task in playbook_data['tasks']:
+                bar.next()
                 PlaybookProcessorService.action_switcher(
                     {
                         "task": task,
-                        "playbook_data": playbook_data,
+                        "taskflow_data": playbook_data,
                         "node_client": node_client,
                         "ftp_client": ftp_client,
                         "node_processor_service": NodeProcessorService 
                     }
                 )
-
-            #    bar.next()
+                
             ftp_client.close()
             node_client.close()
             bar.finish()
+        else:
+            print(f'Unable to connect to node(s) for "{playbook_name}" taskflow.')
         return node_connection
-
-
-        #if node['hostname'] in CONFIGS.LOCAL_NODE_TAGS:
-        #    bar = ChargingBar(
-        #        'Processing {} on {}'.format(playbook_name, node["name"]), 
-        #        max=len(playbook_data['tasks'])
-        #    )
-        #    [PlaybookProcessorService.generate_content_local(task, playbook_data['secrets']) for task in playbook_data['tasks']]
-        #    bar.finish()
-        #else:
-        #    node_connection = node_processor_service.connect_node_with_creds(
-        #        node['hostname'],
-        #        node['username'],
-        #        node['password']
-        #    )
-        #    if node_connection['state']=="ok":
-        #        node_client = node_connection["client"]
-        #        ftp_client = node_client.open_sftp()
-        #        bar = ChargingBar(
-        #            'Processing {} on {}'.format(playbook_name, node["name"]), 
-        #            max=len(playbook_data['tasks'])
-        #        )
-        #        ## For Generation tasks, invoke generator service.
-        #        [PlaybookProcessorService.generate_content_remote(task, playbook_data['secrets'], node_client, ftp_client) for task in playbook_data['tasks']]
-#
-        #        #    bar.next()
-        #        ftp_client.close()
-        #        node_client.close()
-        #        bar.finish()
-        #    return node_connection
 
     @staticmethod
     def validate_playbook_struct(playbook_data):
         if CONFIGS.SECRETS_KEY not in playbook_data:
             playbook_data['secrets'] = None
         return playbook_data
-
-    @staticmethod
-    def call_generate_action(task, playbook_data, node_client, ftp_client, node_processor_service):
-        GeneratorActionService.generate_content_remote(
-            task, 
-            playbook_data['secrets'], 
-            node_client, 
-            ftp_client,
-            node_processor_service
-        )
-
-    @staticmethod
-    def call_execute_action(task, playbook_data, node_client, ftp_client, node_processor_service):
-        ExecutorActionService.execute_command(
-            task,
-            node_client
-        )
 
 
 
