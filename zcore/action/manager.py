bd8288aa8b13ccd91a05d9d40d410a9b4505990f
rRaza Balbale <ribalbale@gmail.com>
2021-10-17 12:06:33 -0500

    Added references to REST api service.

--- zcore/action/manager.py
+++ zcore/action/manager.py
@@ -1,13 +1,15 @@
 from zcore.action.executor_service import ExecutorActionService
 from zcore.action.file_service import FileActionService
 from zcore.action.generator_service import GeneratorActionService
+from zcore.action.rest_api_service import RestApiActionService
 
 
 def Factory(service_name):
     action_services = {
         "generator" : GeneratorActionService,
         "executor" : ExecutorActionService,
-        "file" : FileActionService
+        "file" : FileActionService,
+        "rest_api_caller": RestApiActionService
     }
 
     return action_services[service_name]()
